4e1464998feaafb86106c1817a3a33447032d69e
Brian Ngo <brianngo@localhost.localdomain>
2015-09-27 11:50:58 -0700

    Add shipping_info page

--- includes/languages/english/html_includes/define_shippinginfo.php
+++ includes/languages/english/html_includes/define_shippinginfo.php
@@ -1,3 +1,12 @@
-<p><strong>Shipping Sample Text ...</strong></p>
-<p>This section of text is from the Define Pages Editor located under Tools in the Admin.</p>
-<p>To remove this section of the text, delete it from the Define Pages Editor.</p>
+<body>
+
+<p align="center"><font size="6">
+<div>
+<a href="https://www.visual-you.com/develop/index.php?main_page=page&id=30">Shipping 
+USA</a></div>
+<div>
+<a href="https://www.visual-you.com/develop/index.php?main_page=page&id=29">
+Shipping International </a>
+</div></font></p>
+
+</body>
