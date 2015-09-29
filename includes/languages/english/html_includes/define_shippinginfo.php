50a047eae73497bb74db4ad2595e8198a0a5a741
quangn92 <zdarkz@yahoo.com>
2015-09-29 04:30:17 -0700

    fix link in shippinginfor

--- includes/languages/english/html_includes/define_shippinginfo.php
+++ includes/languages/english/html_includes/define_shippinginfo.php
@@ -2,10 +2,10 @@
 
 <p align="center"><font size="6">
 <div>
-<a href="https://www.visual-you.com/develop/index.php?main_page=page&id=30">Shipping 
+<a href="https://www.visual-you.com/catalog/shipping-usa-ezp-29.html">Shipping 
 USA</a></div>
 <div>
-<a href="https://www.visual-you.com/develop/index.php?main_page=page&id=29">
+<a href="https://www.visual-you.com/catalog/shipping-international-ezp-30.html">
 Shipping International </a>
 </div></font></p>
 
