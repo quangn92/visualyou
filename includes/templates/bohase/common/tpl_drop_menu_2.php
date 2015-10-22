3a3d55aa1f1090b49154bd0d3288544e75d117ab
quangn92 <zdarkz@yahoo.com>
2015-10-21 23:54:51 -0700

    Add sale in header

--- includes/templates/bohase/common/tpl_drop_menu_2.php
+++ includes/templates/bohase/common/tpl_drop_menu_2.php
@@ -29,7 +29,9 @@
           	</li><?php */?>
             <!--Categories Link in Menu-->
 			<?php 
+				$specials_link .= '<li><a class="category-links" href="' . zen_href_link(FILENAME_SPECIALS) . '"><font color="#FF3D3D">SALE</font></a></li>';
 				$new_product_link .= '<li><a class="category-links" href="' . zen_href_link(FILENAME_PRODUCTS_NEW) . '">' . CATEGORIES_BOX_HEADING_WHATS_NEW . '</a></li>';
+				echo $specials_link;
 				echo $new_product_link;
 			?>
 			
